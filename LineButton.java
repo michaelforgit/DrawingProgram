7c7a3aba21f726b8a6927a5fff6d0554e2c95f7d
michaelforgit <michaelforemail@protonmail.com>
2022-12-14 19:44:27 -0600

    fixed undo with first line

--- LineButton.java
+++ LineButton.java
@@ -27,16 +27,20 @@ public class LineButton  extends JButton implements ActionListener {
   private class MouseHandler extends MouseAdapter {
     private int pointCount = 0;
     private Point firstPoint;
+    private boolean debounce = false;
     public void mouseClicked(MouseEvent event) {
     if (++pointCount == 1) {
         lineCommand = new LineCommand(View.mapPoint(event.getPoint()));
         undoManager.beginCommand(lineCommand);
         firstPoint = View.mapPoint(event.getPoint());
+        debounce = true;
     } else if (pointCount == 2) {
+        debounce = false;
         pointCount = 0;
         lineCommand.setLinePoint(View.mapPoint(event.getPoint()));
         drawingPanel.removeMouseListener(this);
         view.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+        undoManager.undo();  // Remove the temp line that was drawn
         undoManager.endCommand(lineCommand);
       }
     }
@@ -47,7 +51,10 @@ public class LineButton  extends JButton implements ActionListener {
   private class MMAdapter extends MouseMotionAdapter {
     public void mouseMoved(MouseEvent event) {
       if (mouseHandler.pointCount == 1) {
-        undoManager.undo();
+        if (mouseHandler.debounce == false) {
+          undoManager.undo();
+        }
+        mouseHandler.debounce = false;
         System.out.println("Should be drawing a line.");
         lineCommand = new LineCommand(mouseHandler.getFirstPoint(), View.mapPoint(event.getPoint()));
         undoManager.beginCommand(lineCommand);
